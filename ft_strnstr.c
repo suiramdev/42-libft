6f6a5bcc93c44b14936cacf4c5f9e52457803d34
Marius Nouchet <mnouchet@e1r6p12.clusters.42paris.fr>
2022-11-09 18:24:31 +0100

    :art: Removed strlen from strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:42:37 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/10/31 14:50:03 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/09 18:13:44 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	little_l;
 	size_t	i;
 	size_t	j;
 
-	little_l = ft_strlen(little);
-	if (little_l <= 0)
+	if (!little[0])
 		return ((char *)big);
 	i = 0;
 	while (i < len && big[i])
@@ -27,7 +25,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		j = 0;
 		while (big[i + j] == little[j] && little[j])
 			j++;
-		if (j == little_l)
+		if (!little[j])
 			return ((char *)big + i);
 		i++;
 	}
