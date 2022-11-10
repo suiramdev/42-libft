0374c6c018c3d633306640ba780a121e30603189
suiramdev <mariusnouchet@gmail.com>
2022-11-10 19:28:19 +0100

    :bug: strlcat no more copies if size is 0

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 23:51:01 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/11/08 13:48:13 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/10 19:27:29 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	i = 0;
-	while (i < size - 1 && src[i])
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
-	}
-	if (size - i > 0)
+		i = 0;
+		while (i < size - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
+	}
 	return (ft_strlen(src));
 }
