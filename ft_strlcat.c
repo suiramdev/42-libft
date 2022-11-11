3cb0be5bdaa9f67be359addfaf0836cb0638e238
Marius Nouchet <mnouchet@e2r9p9.clusters.42paris.fr>
2022-11-11 11:24:03 +0100

    :bug: Somehow fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 00:35:00 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/10/31 15:06:54 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/11 11:23:42 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	src_l = ft_strlen(src);
 	dst_l = ft_strlen(dst);
+	if (dst_l >= size - 1 || size <= 0)
+		return (size + src_l);
 	i = 0;
 	while (i < size - dst_l - 1 && src[i])
 	{
 		dst[i + dst_l] = src[i];
 		i++;
 	}
-	if (size > 0)
-		dst[i + dst_l] = '\0';
-	if (dst_l < src_l)
-		return (src_l + dst_l);
-	return (size + src_l);
+	dst[i + dst_l] = '\0';
+	return (dst_l + src_l);
 }
