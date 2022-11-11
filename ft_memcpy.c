81f6fde570a58b860ada6ba59a4a039b81506044
Marius Nouchet <mnouchet@e2r9p9.clusters.42paris.fr>
2022-11-11 12:08:31 +0100

    :bug: Handle the null parameter cases

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 01:21:48 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/10/28 22:20:38 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/11 12:08:06 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
