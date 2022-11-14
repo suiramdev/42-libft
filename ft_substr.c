19497b5fd83ba5f2a5c433a0bb18e311cae5bfa8
Marius Nouchet <mnouchet@e2r7p14.clusters.42paris.fr>
2022-11-14 15:44:06 +0100

    :bug: Finally allocate the right memory to substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:04:51 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/11/12 13:44:42 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/14 15:42:06 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*output;
 	size_t	i;
 
-	output_len = 0;
 	s_len = ft_strlen(s);
-	if (start <= s_len)
-	{
-		output_len = s_len - start;
-		if (len < output_len)
-			output_len = len;
-	}
-	output = ft_calloc(output_len + 1, sizeof(char));
+	output_len = 0;
+	while (output_len + start < s_len && output_len < len)
+		output_len++;
+	output = malloc(output_len + 1);
 	if (!output)
 		return (NULL);
 	i = 0;
-	while (i < output_len && s[i + start])
+	while (i < output_len && i + start < s_len)
 	{
 		output[i] = s[i + start];
 		i++;
 	}
+	output[i] = '\0';
 	return (output);
 }
