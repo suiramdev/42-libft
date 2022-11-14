954381398c51919dc089d75de85f7050af8066e0
Marius Nouchet <mnouchet@e2r7p14.clusters.42paris.fr>
2022-11-14 16:41:46 +0100

    :art: Use of ft_lstnew for creating new chain

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 01:13:55 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/11/14 00:10:21 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/11/14 15:49:21 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	first = NULL;
 	while (lst)
 	{
-		new = malloc(sizeof(t_list));
+		new = ft_lstnew(f(lst->content));
 		if (!new)
 		{
 			ft_lstclear(&first, del);
@@ -29,7 +29,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		}
 		if (!first)
 			first = new;
-		new->content = f(lst->content);
 		if (prev)
 			prev->next = new;
 		prev = new;
@@ -37,21 +36,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (first);
 }
-
-/*
-void * addOne(void * p) {void * r = malloc(sizeof(int)); *(int*)r = *(int*)p + 1; return (r);}
-
-int	main(void)
-{
-	int tab[] = {0, 1, 2, 3};
-	t_list * l =  ft_lstnew(tab);
-	for (int i = 1; i < 4; ++i)
-		ft_lstadd_back(&l, ft_lstnew(tab + i));
-	t_list * m = ft_lstmap(l, addOne, free);
-	t_list * tmp = l;
-	for (int i = 0; i < 4; ++i)
-	{
-		printf("\n %d == %d", *(int*)tmp->content, i);
-		tmp = tmp->next;
-	}
-}*/
