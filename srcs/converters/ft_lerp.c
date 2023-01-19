f506729d8413125e03838a59ef7c37be71c420c2
suiramdev <mariusnouchet@gmail.com>
2023-01-19 01:43:04 +0100

    :bug: Fixed float conversion of t in lerp

--- srcs/converters/ft_lerp.c
+++ srcs/converters/ft_lerp.c
@@ -6,11 +6,11 @@
 /*   By: mnouchet <mnouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 01:01:48 by mnouchet          #+#    #+#             */
-/*   Updated: 2023/01/19 01:05:47 by mnouchet         ###   ########.fr       */
+/*   Updated: 2023/01/19 01:40:53 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_lerp(int a, int b, int t)
 {
-    return a + (b - a) * (t / 0xff);
+    return a + (b - a) * (float)(t / 255.0f);
 }
